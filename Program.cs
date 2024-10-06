ac5a0e64e48348cee498212273cea49320f70e48
Muhammad Ilyas <75419324+ilyas-cyber@users.noreply.github.com>
2024-10-06 10:05:40 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "This is POC by Muhammad Ilyas");
+
+app.Run();
 
